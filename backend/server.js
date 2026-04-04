3c163a50a9007952a533f768be006170bd83de46
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-04-04 08:52:29 +0500

    setup MongoDB connection and initialize Express server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-const app = express()
-const port = 3000
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/connectDB";
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
