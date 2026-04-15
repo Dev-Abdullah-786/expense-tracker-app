3f4e3f9ce8b0ca5701608105e5e6333f76c92dc4
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-04-15 08:22:52 +0500

    backend: add income controller function

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,35 @@
+import incomeModel from "../model/incomeModel.js";
+
+export async function addIncome(req, res) {
+  const userId = req.user._id;
+  const { description, amount, category, date } = req.body;
+  if (!description || !amount || !category || !date) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const newIncome = new incomeModel({
+      userId,
+      description,
+      amount,
+      category,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Income added successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
