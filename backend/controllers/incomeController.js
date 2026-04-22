25b5d2c6ae9a634d506eed7d06e9561467f68fdd
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-04-22 08:05:12 +0500

    backend: add getIncomeOverview controller for income analytics and statistics

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,7 +1,8 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import XLSX from "xlsx";
 import incomeModel from "../model/incomeModel.js";
+import getDateRange from "../utils/dateFilter.js";
 
 export async function addIncome(req, res) {
   const userId = req.user._id;
@@ -112,30 +113,64 @@ export async function downloadIncomeExcel(req, res) {
   const userId = req.user._id;
   try {
     const income = await incomeModel.find({ userId }).sort({ date: -1 });
-    
+
     const plainData = income.map((inc) => ({
       Description: inc.description,
       Amount: inc.amount,
       Category: inc.category,
       Date: new Date(inc.date).toLocaleDateString(),
     }));
-    
+
     const worksheet = XLSX.utils.json_to_sheet(plainData);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Income Records");
-    
+
     const fileName = `income_${Date.now()}.xlsx`;
     const filePath = path.join(process.cwd(), fileName);
-    
+
     XLSX.writeFile(workbook, filePath);
-    
+
     res.download(filePath, `income_details_${Date.now()}.xlsx`, (err) => {
       if (err) {
         console.error("Download error:", err);
       }
       fs.unlinkSync(filePath);
     });
-    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
+
+export async function getIncomeOverview(req, res) {
+  const userId = req.user._id;
+  const { range = "monthly" } = req.query;
+  const { start, end } = getDateRange(range);
+  try {
+    const incomes = await incomeModel
+      .find({ userId, date: { $gte: start, $lte: end } })
+      .sort({ date: -1 });
+
+    const totalIncome = incomes.reduce((acc, cur) => acc + cur.amount, 0);
+    const averageIncome = incomes.length > 0 ? totalIncome / incomes.length : 0;
+    const numberOfTransactions = incomes.length;
+
+    const recentTransactions = incomes.slice(0, 9);
+
+    res.status(200).json({
+      success: true,
+      message: "Income Overview fetched successfully",
+      data: {
+        totalIncome,
+        averageIncome,
+        numberOfTransactions,
+        recentTransactions,
+        range,
+      },
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({
